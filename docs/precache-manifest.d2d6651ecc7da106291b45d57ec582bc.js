e8148bd40310b3c906e603b0e3e9259927f9e27b
Joaquin <joaquincorteskha@gmail.com>
2020-08-15 02:26:03 -0400

    docs agregado

---
+++ docs/precache-manifest.d2d6651ecc7da106291b45d57ec582bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfd3fc28be7dfb2a5c3c0d0efbf498b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c56158173626d1f9fdd5",
+    "url": "/static/css/main.3c39c44d.chunk.css"
+  },
+  {
+    "revision": "1094a9d0787669c6640b",
+    "url": "/static/js/2.ba604ae7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba604ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c56158173626d1f9fdd5",
+    "url": "/static/js/main.fa7cd203.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
